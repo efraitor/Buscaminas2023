4c58f18c0048c9b2d8196556af4650b387290635
efraitor_ <efraitor.contact@gmail.com>
2023-03-06 16:28:31 +0100

    Added

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -6,12 +6,24 @@ public class Cell : MonoBehaviour
 {
     //Variable que nos permite conocer si un panel tiene una mina o no
     public bool hasMine;
+    //Array en el que guardamos todas las imágenes que corresponden a que no hay una mina en esa celda al pulsar sobre ella
+    public Sprite[] emptyTextures;
+    //Necesitamos la imagen de que hay una mina
+    public Sprite mineTexture;
+
+    //Creamos unas variables donde guardar la posición de la celda concreta
+    int x, y;
 
     // Start is called before the first frame update
     void Start()
     {
         //Le decimos que hay un 15% de posibilidades de que haya una mina en esa celda
         hasMine = (Random.value < 0.15);//Random.value nos da un valor entre 0 y 1. Si se cumple en este caso que ese valor sea menor que 0.15, hasMine será verdadero, si no falso
+        //Variables para recoger la posición inicial de la celda
+        x = (int)this.transform.position.x; //La posición en X de esa celda concreta (la columna)    (int) lo usamos para transformar un float si lo hubiera a número entero
+        y = (int)this.transform.position.y; //La posición en Y de esa celda concreta (la fila)
+        //Metemos esta celda concreta(this) con esa X e Y que hemos obtenido en la posición X e Y correspondiente de ese array de celdas
+        GridHelper.cells[x, y] = this;
     }
 
     // Update is called once per frame
@@ -38,5 +50,19 @@ public class Cell : MonoBehaviour
             //Descubrir todo el área sin minas alrededor de la celda destapada
             //Comprobar si el juego ha acabado o no
         }
+    }
+
+    //Método para cargar las texturas en las celdas
+    public void LoadTexture()
+    {
+
+    }
+
+    //Método de ayuda para saber si la celda ha sido destapada o no, devolviéndonos verdadero o falso
+    public bool IsCovered()
+    {
+        /*Coge del SpriteRenderer de esa celda la textura que está ahí puesta para saber si es un panel o no
+        * En caso de que sea un panel, será verdadero, y sino falso*/
+        return GetComponent<SpriteRenderer>().sprite.texture.name == "panel";
     }
 }
--- Assets/Scripts/GridHelper.cs
+++ Assets/Scripts/GridHelper.cs
@@ -2,17 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//Es una clase de tipo "Helper", un script que nos sirve de apoyo pero no depende de MonoBehaviour
 public class GridHelper : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //Variables para conocer el ancho y el alto de la rejilla
+    public static int w = 15; //ancho de la malla       static significa que sólo habrá por ejemplo en este caso un ancho y un alto de rejilla en todo el juego
+    public static int h = 15; //alto de la malla
+    //Un array donde guardar todas las celdas de nuestro juego
+    public static Cell[,] cells = new Cell[w, h]; //al ser static también me permite acceder a este desde otro script
+    
+    //Método para destapar todas las minas
+    public static void UncoverAllMines()
     {
-        
+        //Bucle para recorrer el array de celdas y que vaya destapando las minas que haya en esta rejilla
+        foreach (Cell c in cells)
+        {
+            //Si esa celda tiene una minas
+            if (c.hasMine)
+            {
+                //Llamar al método que carga la textura de la minas
+
+            }
+        }
+
+        /*for(int f = 0; f < height; f++)
+        {
+            for (int c = 0; c < width; c++)
+            {
+
+            }
+        }*/
     }
 
-    // Update is called once per frame
-    void Update()
+    //Método para saber si hay una mina en una posición dada
+    public static bool HasMineAt(int x, int y) //La posición de la celda
     {
-        
+        //Si estas condiciones se cumplen estaremos dentro de la rejilla
+        if (x >= 0 && y >= 0 && x < w && y < h)
+        {
+            //Vemos que celda hemos seleccionado y guardamos su posición en una variable de tipo celda
+            Cell cell = cells[x, y];
+            //De esa celda nos devolverá su booleano, si es true habrá mina, si es falso no
+            return cell.hasMine;
+        }
+        //Si las condiciones de arriba no se cumplen estamos fuera de la rejilla
+        else
+        {
+            //No hay posibilidad de que haya una mina
+            return false;
+        }
     }
+   
 }
